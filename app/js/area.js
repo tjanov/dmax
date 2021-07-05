01c43f445f09218f5d5354422798eee3bb8cf29e
Imran <okeagnc@gmail.com>
2021-07-05 08:36:09 +0500

    second

--- app/js/area.js
+++ app/js/area.js
@@ -127,3 +127,26 @@ function displayPassword(event) {
         event.target.parentElement.previousSibling.previousSibling.setAttribute('type','password');
     }
 }
+
+form.children("div").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+    labels: {
+        next: "Продолжить",
+        previous: "Назад",
+        finish: "Завершить",
+    },
+    onStepChanging: function (event, currentIndex, newIndex) {
+        form.validate().settings.ignore = ":disabled,:hidden";
+        return form.valid();
+    },
+    onFinishing: function (event, currentIndex) {
+        form.validate().settings.ignore = ":disabled";
+        return form.valid();
+    },
+    onFinished: function (event, currentIndex) {
+        // alert("Submitted!");
+        form.submit();
+    }
+});
