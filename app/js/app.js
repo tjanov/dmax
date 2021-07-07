41b1a88b34cb0f298f5988f38d9f3864a68a478e
Imran <okeagnc@gmail.com>
2021-07-07 15:10:51 +0500

    add createlogo

--- app/js/app.js
+++ app/js/app.js
@@ -88,6 +88,16 @@ $(document).ready(function() {
 
     });
 
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////
